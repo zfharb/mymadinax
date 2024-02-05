cc3bb6711bcb92218c79e230fd76567072b6fa8d
ziad harb <ziadharb@ziads-Air.lan>
2024-02-05 13:21:57 -0600

    modify header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,43 +6,35 @@ function Header() {
   
   return (
       <>
-      <nav class={`navbar fixed-top navbar-expand-lg navbar-dark p-md-3 header-bg`}>
-      <div class="container-fluid">
-        <br/>       
-        <br/>       
-        <br/>    
-              
-          
-        <a class="navbar-brand text-dark fs-1" href="/">myMadinaX</a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent" 
-          aria-controls="navbarSupportedContent" 
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
 
-        <div class="collapse navbar-collapse"  id="navbarSupportedContent">
-          <ul class="navbar-nav position-absolute top-25 end-0">
-            <li class="nav-item">
-              <a class="nav-link text-dark fw-bolder" href="/Home">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link text-dark fw-bolder" href="/Register">Register</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link text-dark fw-bolder" href="/Login">Login</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-    
-    </>
+        <nav class="navbar  navbar-expand-lg navbar-light bg-light">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#">Bizyaab</a>
+            <button class="navbar-toggler" type="button" 
+                    data-bs-toggle="collapse" 
+                    data-bs-target="#navbarSupportedContent" 
+                    aria-controls="navbarSupportedContent" 
+                    aria-expanded="false" 
+                    aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav mb-2 mb-lg-0 ms-auto">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Singin</a>
+                </li>
+                <li class="nav-item ">
+                  <a class="nav-link" href="#" tabindex="-1" aria-disabled="true">Sing up</a>
+                </li>
+              </ul>
+             
+            </div>
+          </div>
+        </nav>
+      </>
     )
   }
   
